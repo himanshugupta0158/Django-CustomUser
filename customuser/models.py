c9a6c57a4adeda13e224b6ed9ad1f1d3bfac3363
himanshugupta0158 <himanshugupta0158@gmail.com>
2021-12-22 11:25:17 +0530

    customusermanager added

--- customuser/models.py
+++ customuser/models.py
@@ -2,52 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy
 # below is used to make changes in admin model
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .customusermanager import CustomAccountManager
+
 
 # Create your models here.
-
-class CustomAccountManager(BaseUserManager):
-
-    def create_superuser(self , email , user_name , firstname , lastname , password , **other_fields):
-        
-        other_fields.setdefault('is_staff' , True)
-        other_fields.setdefault('is_superuser' , True)
-        other_fields.setdefault('is_active' , True)
-        
-        if other_fields.get('is_staff') is not True :
-            raise ValueError(
-                'Superuser must be assigned to is_staff=True.'
-            )
-        if other_fields.get('is_superuser') is not True :
-            raise ValueError(
-                'Superuser must be assigned to is_superuser=True.'
-            )
-        
-        return self.create_user(email , user_name , firstname ,lastname ,password , **other_fields)
-        
-    def create_user(self , email , user_name , firstname , lastname , password , **other_fields):
-        
-        # validating email
-        if not email :
-            raise ValueError(gettext_lazy('You must provide an email address'))
-        
-        
-        # all email letter from capital to small letters
-        email = self.normalize_email(email)
-        # assigning values in user's model
-        user = self.model(email=email , 
-                          user_name=user_name , 
-                          firstname = firstname,
-                          lastname=lastname,
-                          **other_fields
-                          )
-        # setting password
-        user.set_password(password)
-        # now, saving user 
-        user.save()
-        # return saved user
-        return user
-    
 
 # added permissionmixin for adding user permission of that of a admin
 class NewUser(AbstractBaseUser , PermissionsMixin):
